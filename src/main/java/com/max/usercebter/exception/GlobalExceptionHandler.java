67d92ca8ba1c2b85c7bc30816622bf2824c58351
MAX <61301100+miaoxu404@users.noreply.github.com>
2025-08-30 15:24:19 +0800

    优化全局异常

--- src/main/java/com/max/usercebter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/max/usercebter/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.max.usercebter.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.max.usercebter.common.BaseResponse;
 import com.max.usercebter.common.ErrorCode;
 import com.max.usercebter.common.ResultUtils;
@@ -7,10 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(BusinessException.class)
     public BaseResponse businessExceptionHandler(BusinessException e) {
         log.error("businessException: {}", e.getMessage(), e);
